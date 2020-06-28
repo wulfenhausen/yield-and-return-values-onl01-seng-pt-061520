106ae538815f5a8fc2c24a239cdcb941abceb8da
William Han Findiesen <william.findiesen@gmail.com>
2020-06-28 18:57:51 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,8 +1,8 @@
 def hello(array)
   i = 0
-  while i < array.length
-    yield(array[i])
+  collection = []
+    while i < array.length
+    collection << yield(array[i])
     i += 1
   end
 end
-binding.pry
